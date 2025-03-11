321cb22260435be18e13a279e613e824917327a1
Cleyton Alves <cleyton.alves.a@gmail.com>
2025-03-11 19:20:34 -0300

    feat: :sparkles: Estrutura do handler de erro

---
+++ src/main/java/com/dio/barbershopapi/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.dio.barbershopapi.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+}
